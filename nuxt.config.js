146e7b3cf73ef7d74faa7afe908613d7e7108555
Raycon Lima <rayconlimabatista@gmail.com>
2022-01-16 00:37:40 -0400

    eleven Commit - adding jsonld

--- nuxt.config.js
+++ nuxt.config.js
@@ -140,9 +140,7 @@ export default {
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-    './plugins/repository'
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
